2ff609e28c2ffd753167733c12b818204625b298
Volodymyr Kovalenko <380974261314kv@gmail.com>
2024-04-01 23:45:02 +0300

    Configured urls.py, added index view, created base.html, index.html templates to display the home page

---
+++ restaurant/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from restaurant.views import (
+    index,
+)
+
+
+app_name = "restaurant"
+
+urlpatterns = [
+    path("", index, name="index"),
+]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from restaurant.models import Dish, DishType, Cook
+
+
+def index(request: HttpRequest) -> HttpResponse:
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits,
+    }
+
+    return render(request, "restaurant/index.html", context=context)
